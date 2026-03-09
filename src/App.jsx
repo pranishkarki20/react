aca8463b4e4b02256b58fbe14922b542a735deaa
pranishkarki20 <praniskkarki20@gmail.com>
2026-03-09 18:07:56 +0530

    jhkla

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
-import './App.css'
-import Navbar from "./components/Navbar"
+import './App.css';
+import Navbar from "./components/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./components/login";
+import Login from "./components/Login";
 
 function App() {
-  return(
+  return (
     <BrowserRouter>
-    <Routes>
-    <Route path = "/" element = {
-      <>
-      <Navbar /> 
-      </>
-    }
-      />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Navbar />
+            </>
+          }
+        />
         <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
