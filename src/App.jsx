288e834e4474b80307a8cdef6f5d23cf4c921158
pranishkarki20 <praniskkarki20@gmail.com>
2026-03-09 18:01:17 +0530

    push

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import Navbar from "./components/Navbar"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/login";
+
+function App() {
+  return(
+    <BrowserRouter>
+    <Routes>
+    <Route path = "/" element = {
+      <>
+      <Navbar /> 
+      </>
+    }
+      />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App;
