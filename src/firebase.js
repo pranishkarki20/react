456fe865a3229dbebf4584e063c686a8e5cc817f
Pranish Karki <praniskkarki20@gmail.com>
2026-03-13 15:59:32 +0530

    Remove Firebase setup from firebase.js
    Removed Firebase initialization and configuration code.

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,1 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAYRPBDWnaX5pmiQfFCUeFz3FoSEipd2mA",
-  authDomain: "kinara-56786.firebaseapp.com",
-  projectId: "kinara-56786",
-  storageBucket: "kinara-56786.firebasestorage.app",
-  messagingSenderId: "1052088542324",
-  appId: "1:1052088542324:web:809c9844ad2b6a95eddd21"
-};
-
-const app = initializeApp(firebaseConfig);   // initialize firebase
-
-export const auth = getAuth(app);            // create auth instance
