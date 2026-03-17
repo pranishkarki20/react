d35949a02df1af14934e55b5f63e08ea8784bbc5
pranishkarki20 <praniskkarki20@gmail.com>
2026-03-17 19:08:40 +0530

    logon

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -39,7 +39,7 @@ function Login() {
     <form onSubmit={handleSubmit}>
       
       <motion.div className="container1" id="login"
-      initial={{opacity:0 , y : -50}}
+      initial={{opacity:0 , y : 50}}
        animate = {{opacity : 1 , y :0}}
       transition = {{duration:1}}
       >
