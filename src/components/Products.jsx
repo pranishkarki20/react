1b2bed1e30dc7451615e9c708e6c48d992a1d497
pranishkarki20 <praniskkarki20@gmail.com>
2026-03-19 10:31:13 +0530

    prdcur to category

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,49 +1,32 @@
+import { link } from 'framer-motion/client';
 import './product.css';
-import {motion} from "framer-motion";
-export default function product() {
-    return(
-        <section id = "product" className='product'>
-            <div className="product-container">
-           <h1>Featued Products</h1>
-           <div className = "Productsitems">
+import { motion } from "framer-motion";
 
-             <div className = "product1"> 
-                <h1>coke</h1>
-                   <motion.button 
-                whileHover={{scale: 1.1}}
-                type = 'submit'
-                className = 'ppbtn'> Buy Now </motion.button>
-            </div >
-              <div className = "product2">  
-                <h1>bana</h1>
-                <motion.button 
-                whileHover={{scale: 1.1}}
-                type = 'submit'
-                className = 'ppbtn'> Buy Now </motion.button>
-             </div>
-             <div className='product3'>
-                <h1>Laze</h1>
-             <motion.button 
-                whileHover={{scale: 1.1}}
-                type = 'submit'
-                className = 'ppbtn'> Buy Now </motion.button>
-             </div>
-              <div className='product4'>
-                <h1>Bingo</h1>
-             <motion.button 
-                whileHover={{scale: 1.1}}
-                type = 'submit'
-                className = 'ppbtn'> Buy Now </motion.button>
-             </div>
+export default function Product() {
 
-           </div>
-          <motion.button 
-          whileHover={{
-            scale: 1.1
-          }}
-          type='submit'
-          className='pbtn' >View All Products</motion.button>
-         </div>
-        </section>
-    )
+  const productitems = [
+    { id: 1, name: "Electronics" , link :"/"},
+    { id: 2, name: "Clothes" , link :"/" },
+    { id: 3, name: "Kitchen" , link :"/" },
+    { id: 4, name: "Jewellery" , link :"/" }
+  ];
+
+  return (
+    <section id="product" className="product">
+      <div className="product-container">
+        
+        <h1>Featured Products</h1>
+
+        <div className="Productsitems">
+          <div className = "category">
+          {productitems.map((item) => (
+            <a href = {item.link}>
+            < motion.div className="card"   whileHover ={{scale: 1.1}}
+            key={item.id}>{item.name}</motion.div></a>
+          ))}
+        </div>
+</div>
+      </div>
+    </section>
+  );
 }
