2787711126636823d01c2bfdc28778a747c4263c
pranishkarki20 <praniskkarki20@gmail.com>
2026-03-21 23:33:34 +0530

    update

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -4,11 +4,11 @@ import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import './Product.css'
 export default function Product(){
     const product = [
-        {id : 1 , name: "Macbook"  , image: "/mac.jpg"},
-        {id : 2 , name : "Iphone" ,   image: "/mac.jpg"},
-        {id : 3 , name : "Suit",  image: "/mac.jpg"},
-        {id : 4 , name : "Check shirt",image:"/checkshirt.jpg" },
-        {id: 5 , name : "Bag" , image:"/bag.jpg"}
+        {id : 1 , name: "Macbook"  , image: "/mac.jpg" , price: "$1000"},
+        {id : 2 , name : "Iphone" ,   image: "/iphone.jpg" ,price: "$599"},
+        {id : 3 , name : "Suit",  image: "/suit.jpg" ,price: "$250"},
+        {id : 4 , name : "Check shirt",image:"/checkshirt.jpg" , price: "$100" },
+        {id: 5 , name : "Bag" , image:"/bag.jpg" , price: "$89.90"}
     ]
     return(
     <section id="Products" className="Products">
@@ -24,11 +24,12 @@ export default function Product(){
                 className = "Card">
                      <img src={iteam.image}  alt={iteam.name} className='imges' />
                 <div className = "text">{iteam.name}</div>
+                <div className='Price'>{iteam.price}</div>
                 <Button 
                 className='button'
                 variant='contained' startIcon = {<ShoppingCartIcon/>} 
                 style={{
-                    margin: "0 auto",
+                    margin: "10px auto",
                     display :"flex",
                     justifyItems : "center",
                     color:"black",
@@ -43,6 +44,13 @@ export default function Product(){
             ))
             }
                 </div>
+                 <Button variant='outlined'  sx ={{
+                    margin:"15px auto",
+                    color:"black",
+                    bordercolor:"black"
+                 }}>
+                    View all products
+            </Button>
         </div>
     </section>
     )
