f6d06b778101cbc8a55107378dcf7f02592f4dcc
pranishkarki20 <praniskkarki20@gmail.com>
2026-03-22 23:24:28 +0530

    Home button update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,12 +2,16 @@ import './Css.css';
 import{motion} from "framer-motion";
 export default function Home(){
 return (
+    <div>
     <section id = "home" className = "home">
     <motion.div className="home-container"
     initial={{ opacity: 0, y: 50 }}
     animate={{ opacity: 1, y: 0 }}
     transition={{ duration: 2 }}
     >
+           <video autoPlay loop muted playsInline className="bg-video">
+        <source src = "/product.mp4"/>
+    </video>
     <h1>Products</h1>
     <motion.button
     whileHover={{
@@ -18,6 +22,7 @@ return (
     }}
     className='btn' >Shop</motion.button>
     </motion.div>
-    </section>
+    
+    </section></div>
 )
 }
